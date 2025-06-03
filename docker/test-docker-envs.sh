ae665bc8d3085d2abd7fe0d79d4cdbc40aa1ca93
ToriK17 <vfcad@protonmail.com>
2025-06-02 20:51:47 -0600

    add docker environments for linux distro testing

--- docker/test-docker-envs.sh
+++ docker/test-docker-envs.sh
@@ -7,3 +7,5 @@ for distro in debian alpine fedora; do
   docker build -t edr-test-$distro -f docker/$distro/Dockerfile .
   docker run --rm edr-test-$distro
 done
+# The Fedora container requires --security-opt seccomp=unconfined due to dnf’s syscall usage.
+# This works for locally simulating activity, never for production
